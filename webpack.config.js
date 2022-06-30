9b636a0c8865962593f02c75cfd4525d3b1254e8
Jasminezalez <jasminezalez@gmail.com>
2022-06-29 19:10:54 -0700

    setup webpack and initial project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = { 
+    mode: process.env.NODE_ENV,
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /.(css|scss)$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass'],
+            }
+        ],
+    },
+    
+    devServer: {
+        static: {
+            publicPath: '/',
+            directory: path.resolve(__dirname, './public')
+        },
+        proxy: {
+            '/api': 'http://localhost:3000'
+        },
+        // host: 'localhost',
+        // port: 8080
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
