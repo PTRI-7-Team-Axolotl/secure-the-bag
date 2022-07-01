54b804e4821b58363e707753afcc72d10c8eaf27
Michael C Grant <Michael.Collier.Grant@gmail.com>
2022-06-30 23:21:31 -0500

    updated server boilerplate

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,49 @@
+require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
-require('dotenv').config();
+const PORT = process.env.PORT;
+const userRouter = require('./routes/userRoutes');
+const authRouter = require('./controllers/authController');
 
-// Serve static files
-app.use('/', express.static(path.resolve(__dirname, '../build')));
+// Parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// Serve index
-app.get('/*', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../build/index.html')));
+// Don't serve static files or homepage from Dev Server
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files
+  app.use('/', express.static(path.resolve(__dirname, '../build')));
+  // Serve index
+  app.get('/*', (req, res) =>
+    res.status(200).sendFile(path.resolve(__dirname, '../build/index.html'))
+  );
+}
+
+// Routes
+app.use('/users', userRouter);
+app.use('/auth', authRouter);
+
+// 404 Catch-All
+app.use('*', (req, res) => res.status(404).send('Not Found'));
+
+// Universal Error Handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error.',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.error(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 // Start Server
-app.listen(process.env.PORT, () => {
-    console.log(`server is running on port ${process.env.PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
