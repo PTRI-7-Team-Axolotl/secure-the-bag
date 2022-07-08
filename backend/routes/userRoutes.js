756b1ccd836597d0c939bb78875ffbddf929e2f5
Daniel Aurand <daurand303@gmail.com>
2022-07-07 19:25:29 -0600

    final touches on backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 // this gets a job that the user has saved
-router.get("/job/:id",  authController.verifyUser, userController.getJob, (req, res) => {
+router.get("/job/:id",  authController.verifySession, userController.getJob, (req, res) => {
   return res.status(200).json(res.locals.values);
 });
 
 // this will get all of the saved jobs
 router.get(
-  "/getalljobs",
+  "/getalljobs", authController.verifySession,
   userController.getAllJobs,
   (req, res) => {
     console.log("attempting to get all jobs");
@@ -20,7 +20,7 @@ router.get(
 
 // this will save a job when a user swipes right
 router.post(
-  "/savejob",
+  "/savejob", authController.verifySession, 
   userController.saveJob,
   (req, res) => {
     console.log("inside userRoute");
