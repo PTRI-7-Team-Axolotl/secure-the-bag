98b9d21377934d6647ff16d0f0334e87a9db2d5e
Jasminezalez <jasminezalez@gmail.com>
2022-07-08 09:28:14 -0700

    no changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
+const authController = require("../controllers/authController")
 
 // this gets a job that the user has saved
 // :id parameter required
-router.get("/getjob", userController.getJob, (req, res) => {
+router.get("/getjob", authController.verifyUser, userController.getJob, (req, res) => {
   return res.status(200).json(res.locals.values);
 });
 
 // this will get all of the saved jobs
-router.get("/getalljobs", userController.getAllJobs, (req, res) => {
+router.get("/getalljobs", authController.verifyUser, userController.getAllJobs, (req, res) => {
   console.log("attempting to get all jobs");
   return res.status(200).json(res.locals.values);
 });
 
-router.post("/savejob", userController.saveJob, (req, res) => {
+router.post("/savejob", authController.verifyUser, userController.saveJob, (req, res) => {
   console.log("inside userRoute");
   return res.status(200).json(res.locals.user);
 });
