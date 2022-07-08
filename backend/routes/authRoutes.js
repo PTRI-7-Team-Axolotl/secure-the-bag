be33bd8a12e3c5084b89461818ab77a00a2a30cc
Daniel Aurand <daurand303@gmail.com>
2022-07-07 19:06:58 -0600

    final touches on backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const authController = require('../controllers/authController')
 
 console.log('inside authRoutes.js')
-// add encryption middleware before signup when created (Mike)
 router.post('/signup', authController.encryptPassword, userController.signup, (req, res) => {
     console.log('im inside auth Router!!')
     return res.status(200).json(res.locals.user_id)
 })
 
 router.post('/login', authController.verifyUser, (req, res) => {
-
     return res.status(200).json(res.locals.verified_id)
 })
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
+const authController = require("../controllers/authController");
 
 // this gets a job that the user has saved
-// :id parameter required
-router.get("/getjob", userController.getJob, (req, res) => {
+router.get("/job/:id",  authController.verifyUser, userController.getJob, (req, res) => {
   return res.status(200).json(res.locals.values);
 });
 
 // this will get all of the saved jobs
-router.get("/getalljobs", userController.getAllJobs, (req, res) => {
-  console.log("attempting to get all jobs");
-  return res.status(200).json(res.locals.values);
-});
+router.get(
+  "/getalljobs",
+  userController.getAllJobs,
+  (req, res) => {
+    console.log("attempting to get all jobs");
+    return res.status(200).json(res.locals.values);
+  }
+);
 
-router.post("/savejob", userController.saveJob, (req, res) => {
-  console.log("inside userRoute");
-  return res.status(200).json(res.locals.user);
-});
+// this will save a job when a user swipes right
+router.post(
+  "/savejob",
+  userController.saveJob,
+  (req, res) => {
+    console.log("inside userRoute");
+    return res.status(200).json(res.locals.user);
+  }
+);
 
 module.exports = router;
